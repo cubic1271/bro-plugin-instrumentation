7feedc9352136acbd483420ff03f19addf6bcfa2
Gilbert Clark <gjclark1@localhost.localdomain>
2015-06-18 06:20:40 -0400

    Small tweaks and regenerated bin directory.

--- ui/server.sh
+++ ui/server.sh
@@ -5,7 +5,7 @@ SCRIPTDIR=`dirname $BROPATH`/../share/bro
 
 grunt
 pushd bin
-rm -f ./bro && ln -s $SCRIPTDIR bro
+# rm -f ./bro && ln -s $SCRIPTDIR bro
 cp ../data/bro-profile-function ./profile.json
 cp ../data/bro-profile-collection ./collection.json
 cp ../data/callgraph.png ./callgraph.png
